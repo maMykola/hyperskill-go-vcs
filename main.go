29daf06b6a1634e3c11376a870e2967582833dbe
Mykola Martynov <mykola.martynov@hotmail.com>
2024-07-05 09:15:19 +0300

    Stage 1

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type Command string
+
+const (
+	cmdConfig   Command = "config"
+	cmdAdd      Command = "add"
+	cmdLog      Command = "log"
+	cmdCommit   Command = "commit"
+	cmdCheckout Command = "checkout"
+)
+
+var allowedCommands = []Command{
+	cmdConfig,
+	cmdAdd,
+	cmdLog,
+	cmdCommit,
+	cmdCheckout,
+}
+
+var helpMsg = map[Command]string{
+	cmdConfig:   "Get and set a username.",
+	cmdAdd:      "Add a file to the index.",
+	cmdLog:      "Show commit logs.",
+	cmdCommit:   "Save changes.",
+	cmdCheckout: "Restore a file.",
+}
+
+func main() {
+	if len(os.Args) < 2 || os.Args[1] == "--help" {
+		displayHelp()
+		return
+	}
+
+	var cmd = Command(os.Args[1])
+
+	if help, ok := helpMsg[cmd]; ok {
+		fmt.Println(help)
+	} else {
+		fmt.Printf("'%s' is not a SVCS command.\n", cmd)
+	}
+}
+
+func displayHelp() {
+	fmt.Println("These are SVCS commands:")
+	for _, command := range allowedCommands {
+		fmt.Printf("%-8s  %s\n", command, helpMsg[command])
+	}
+}
