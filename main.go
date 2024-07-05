2de41d0b535beeb3c96ae1d76c43c827e82a343b
Mykola Martynov <mykola.martynov@hotmail.com>
2024-07-05 09:43:02 +0300

    Stage 2: implement config

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
@@ -13,6 +14,11 @@ const (
 	cmdLog      Command = "log"
 	cmdCommit   Command = "commit"
 	cmdCheckout Command = "checkout"
+)
+
+const (
+	vcsDir     = "./vcs/"
+	configFile = vcsDir + "config.txt"
 )
 
 var allowedCommands = []Command{
@@ -38,10 +44,20 @@ func main() {
 	}
 
 	var cmd = Command(os.Args[1])
+	var args = os.Args[2:]
 
-	if help, ok := helpMsg[cmd]; ok {
-		fmt.Println(help)
-	} else {
+	switch cmd {
+	case cmdConfig:
+		config(args)
+	case cmdAdd:
+		add(args)
+	case cmdLog:
+		log(args)
+	case cmdCommit:
+		commit(args)
+	case cmdCheckout:
+		checkout(args)
+	default:
 		fmt.Printf("'%s' is not a SVCS command.\n", cmd)
 	}
 }
@@ -51,4 +67,57 @@ func displayHelp() {
 	for _, command := range allowedCommands {
 		fmt.Printf("%-8s  %s\n", command, helpMsg[command])
 	}
+}
+
+func config(args []string) {
+	if len(args) == 1 {
+		setUsername(args[0])
+	}
+
+	if username, err := getUsername(); err == nil {
+		fmt.Printf("The username is %s\n", username)
+	} else {
+		fmt.Println("Please, tell me who you are.")
+	}
+}
+
+func setUsername(username string) {
+	os.MkdirAll(vcsDir, os.ModePerm)
+
+	file, err := os.Create(configFile)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	file.WriteString(username)
+}
+
+func getUsername() (string, error) {
+	file, err := os.Open(configFile)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+
+	return scanner.Text(), scanner.Err()
+}
+
+func add(args []string) {
+	// todo: stub
+}
+
+func log(args []string) {
+	// todo: stub
+}
+
+func commit(args []string) {
+	// todo: stub
+}
+
+func checkout(args []string) {
+	// todo: stub
 }
